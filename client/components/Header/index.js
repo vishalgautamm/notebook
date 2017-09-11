9e28d0c47334fefb778fd25ea9397e6837f16608
vishalgautamm <vishal.g@live.ca>
2017-09-11 14:52:56 -0400

    Styled nav section

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import './Header.css'
+
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-const Header = () => <div> This is a sample NavBar </div>
+class Header extends Component {
+	render() {
+		return (
+			<header className="Header nav-wrapper">
+				<div className="Header brand-logo">
+					<Link to="/"> Notebook </Link>
+				</div>
+				<ul className="Header nav">
+					<li className="nav-item selected">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/messages">Messages</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/"> Sign up</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/"> Sign in</Link>
+					</li>
+				</ul>
+			</header>
+		)
+	}
+}
 
 export default Header
