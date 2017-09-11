05990feec982116193d541ae10967c32ef5a5397
vishalgautamm <vishal.g@live.ca>
2017-09-11 12:00:16 -0400

    Added documentation for tests

--- server/models/__tests_/notes_test.js
+++ server/models/__tests_/notes_test.js
@@ -1,5 +1,15 @@
 const Note = require('../Notes')
 const assert = require('assert')
+
+// TO DO: 6 TESTS
+// - Need to implement the following tests:
+// - Creating a new comment (DONE)
+// - reading every comments (DONE)
+// - Reading comment by id
+// - Deleting comment by id
+// - Deleting entire thread of comments
+// - Updating a comment
+
 // Creating a comment test
 describe('Creating comment records', () => {
 	it('Save a comment doc to the database', done => {
